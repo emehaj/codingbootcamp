e6864108367b67033c51e4fd36bc0c3f9b85aff8
emehaj <emehaj@emehaj.com>
2026-02-03 23:10:12 +0100

    fix: layout and style issues in slider component

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const tanyaQ = document.querySelector(".tanyaQ");
 const johnQ = document.querySelector(".johnQ");
 const buttons = document.querySelectorAll("button");
-const profileImages = document.querySelectorAll(".img-container > img");
+const profileImages = document.querySelectorAll(".slider > img");
+
+const imageContainer = document.querySelector(".img-container");
+const slider = document.querySelector(".slider");
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         e.preventDefault();
 
         if (btn.className === "prev") {
-            offset = profileImages[0].clientWidth + 20;
-            tanyaQ.classList.add("hide");
-            johnQ.classList.add("show");
-        } else {
-            offset = 0;
+            slider.style.left = "0px";
             tanyaQ.classList.remove("hide");
             johnQ.classList.remove("show");
+        } else {
+            slider.style.left = "calc(-100% - 10px)";
+            tanyaQ.classList.add("hide");
+            johnQ.classList.add("show");
         }
-
-        profileImages.forEach((img) => {
-            img.style.transform = `translateX(${-offset}px)`;
-        });
     });
 });
